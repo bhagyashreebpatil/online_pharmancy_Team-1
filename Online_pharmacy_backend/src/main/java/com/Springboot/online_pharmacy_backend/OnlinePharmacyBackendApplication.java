fc9023dd6e2588c43d344182fee9d6b944445807
Bhagyashree b patil <bhagyashreebpatil029@gmail.com>
2025-08-07 18:28:46 +0530

    Creating springboot project

---
+++ Online_pharmacy_backend/src/main/java/com/Springboot/online_pharmacy_backend/OnlinePharmacyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot.online_pharmacy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePharmacyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePharmacyBackendApplication.class, args);
+	}
+
+}
