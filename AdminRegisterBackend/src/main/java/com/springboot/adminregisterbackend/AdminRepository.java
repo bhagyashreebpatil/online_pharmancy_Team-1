a238fd64737a1fc300b03e6d545785e6cc98ac70
shravani-0803754 <shravanib639@gmail.com>
2025-08-10 17:17:46 +0530

    Delete AdminRegisterBackend directory

--- AdminRegisterBackend/src/main/java/com/springboot/adminregisterbackend/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.adminregisterbackend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-	  boolean existsByEmail(String email);
-	}
